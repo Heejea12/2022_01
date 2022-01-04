9f607d916396ee776d8271df5ad3270cb45c18b0
simeddk <65766852+simeddk@users.noreply.github.com>
2022-01-04 20:58:54 +0900

    0104_HelloWorld

---
+++ BasicSynytax/BasicSynytax/Main.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+int main()
+{
+	cout << "Hello" << endl;
+	system("pause");
+
+	return 0;
+}
---
+++ BasicSynytax/BasicSynytax/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+#define TEXTCOLOR_RED SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+#define TEXTCOLOR_GREEN SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+#define TEXTCOLOR_BLUE SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+#define TEXTCOLOR_CYAN SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  FOREGROUND_GREEN | FOREGROUND_BLUE);
+#define TEXTCOLOR_MAGENTA SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  FOREGROUND_RED | FOREGROUND_BLUE);
+#define TEXTCOLOR_YELLOW SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  FOREGROUND_RED | FOREGROUND_GREEN);
+#define TEXTCOLOR_WHITE SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+#define PrintFile TEXTCOLOR_GREEN; \
+cout << __FILE__ << endl; \
+cout << __FUNCTION__ << endl; \
+TEXTCOLOR_WHITE;
