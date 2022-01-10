b203d1c7e88a0893612fb0f4e160cfd72b5c2fc7
simeddk <65766852+simeddk@users.noreply.github.com>
2022-01-10 20:57:55 +0900

    0110_CustomizationType

--- BasicSynytax/BasicSynytax/Main.cpp
+++ BasicSynytax/BasicSynytax/Main.cpp
@@ -1,9 +1,13 @@
 #include "stdafx.h"
 #include "01_Variable/VariableDemo.h"
+#include "01_Variable/ByteSizeDemo.h"
+#include "01_Variable/EnumDemo.h"
+#include "01_Variable/TypedefDemo.h"
+#include "01_Variable/StructDemo.h"
 
 int main()
 {
-	Execute();
+	StructDemo::Execute();
 
 	
 	system("pause");
